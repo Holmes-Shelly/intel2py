9d33747c426dc2f6265610c30eb72574e1d4e900
Shi Hao <shihao06@gmail.com>
2019-03-14 10:00:09 +0800

    Add files via upload

--- test.py
+++ test.py
@@ -1,15 +1,14 @@
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 import time
-url_login = 'https://accounts.google.com/'
+url_login = 'https://accounts.google.com/ServiceLogin?service=ah&passive=true&continue=https://appengine.google.com/_ah/conflogin%3Fcontinue%3Dhttps://intel.ingress.com/intel'
 chrome_options = webdriver.ChromeOptions()
 # chrome_options.set_headless()
 chrome_options.add_argument('--no-sandbox') 
 chrome_options.add_argument('--headless')
 chrome_options.add_argument('--disable-gpu')
-	
-driver = webdriver.Chrome(options=chrome_options)
-# driver = webdriver.Chrome()
+driver = webdriver.Chrome(driver_path='/usr/bin/chromedriver', chrome_options=chrome_options, service_args=['--verbose', '--log-path=/tmp/chromedriver.log'])
+# driver = webdriver.Chrome(options=chrome_options)
 driver.get(url_login)
 
 #get the username textbox
@@ -28,14 +27,12 @@ password_button = driver.find_element_by_id("signIn")
 
 password_field.send_keys(u'shihao1992!G')
 # password_button.click
-password_button.send_keys(Keys.ENTER)
-# js = 'document.getElementById("signIn").click();'
-# driver.execute_script(js)
+# password_button.send_keys(Keys.ENTER)
+js = 'document.getElementById("signIn").click();'
+driver.execute_script(js)
 print "password sended"
 time.sleep(10)
-f = open('0314.html', 'w')
-f.write(driver.page_source.encode('utf-8'))
-f.close
+
 cookies = driver.get_cookies()
 print "cookies got"
 
@@ -43,8 +40,9 @@ for cookie in cookies:
 	if ((cookie['name'] == "SACSID") or (cookie['name'] == "csrftoken")):
 		print cookie['value']
 		
-
+f = open('0314.html', 'w')
+f.write(driver.page_source.encode('utf-8'))
+f.close
 
 driver.quit()
-	
 	
