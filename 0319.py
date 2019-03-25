3b974f12510829ff039f92bb776585e67646f8c7
Shi Hao <shihao06@gmail.com>
2019-03-25 11:02:39 +0800

    Add files via upload

--- 0319.py
+++ 0319.py
@@ -74,9 +74,12 @@ def get_updates():
 		tg_send((),'Update failed, please try it later.')
 		return
 		
-	if("text" in rece_cmd[-1]["message"]):
-		cmd_text = rece_cmd[-1]["message"]["text"]
-		cmd_id = rece_cmd[-1]["message"]["message_id"]
+	#non text message
+	if not("text" in rece_cmd[-1]["message"]):
+		return
+	
+	cmd_text = rece_cmd[-1]["message"]["text"]
+	cmd_id = rece_cmd[-1]["message"]["message_id"]
 		
 	if((re.match(cmd_pattern, cmd_text)) and (cmd_id != last_id)):
 		last_id = cmd_id
@@ -88,6 +91,8 @@ def get_updates():
 			portal_list_del(int(cmd_text[5:]))
 		elif re.match(help_pattern, cmd_text):
 			send_tg((), "I'm your father.")
+		elif re.match(att_pattern, cmd_text):
+			send_tg((), "Maybe I'm not SelfHelp's father, but I still love her.@selfhelp233")
 		else:
 			send_tg((), 'Sorry, your application has been rejected.')
 	return
@@ -226,6 +231,7 @@ cmd_pattern = r'\/.*'
 help_pattern = r'\/help'
 add_pattern = r'\/add\s\w{32}\.\d{2}'
 del_pattern = r'\/del\s\d'
+att_pattern = r'\/attack'
 
 TOKEN = "33637785666:AAHRW-gz-CeKkSGbP_xKubcau0dO28ffBYc"
 url_tg = "https://api.telegram.org/bot{}/".format(TOKEN[2:])
